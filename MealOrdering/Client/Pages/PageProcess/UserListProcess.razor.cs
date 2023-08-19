43200e6354f609e884f036bd736e1c6b54984696
Egemen Unsur <sabitozbilgic2306@gmail.com>
2023-08-19 16:14:03 +0300

    dropdown handled

---
+++ MealOrdering/Client/Pages/PageProcess/UserListProcess.razor.cs
@@ -0,0 +1,85 @@
+﻿using MealOrdering.Client.Utils;
+using MealOrdering.Shared.CustomExceptions;
+using MealOrdering.Shared.DTO;
+using MealOrdering.Shared.ResponseModels;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace MealOrdering.Client.Pages.Users
+{   //Razor sayfasında code bölümünü buraya tasidik.
+    public class UserListProcess: ComponentBase
+    {
+        [Inject]
+        public HttpClient Client { get; set; }
+
+        [Inject]
+        ModalManager ModalManager { get; set; }
+
+        [Inject]
+        NavigationManager NavigationManager { get; set; }
+
+        protected List<UserDTO> UserList = new List<UserDTO>();
+
+
+        protected async override Task OnInitializedAsync()
+        {
+            await LoadList();
+        }
+
+
+        protected void goCreateUserPage()
+        {
+            NavigationManager.NavigateTo("/users/add");
+        }
+
+        protected void goUpdateUserPage(Guid UserId)
+        {
+            NavigationManager.NavigateTo("/users/edit/" + UserId);
+        }
+
+        protected async Task DeleteUser(Guid Id)
+        {
+            bool confirmed = await ModalManager.ConfirmationAsync("Confirmation", "User will be deleted. Are you sure?");
+
+            if (!confirmed) return;
+
+            try
+            {
+                bool deleted = await Client.PostGetServiceResponseAsync<bool, Guid>("api/User/Delete", Id, true);
+
+                await LoadList();
+            }
+            catch (ApiException ex)
+            {
+                await ModalManager.ShowMessageAsync("User Deletion Error", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                await ModalManager.ShowMessageAsync("An Error", ex.Message);
+            }
+        }
+
+        protected async Task LoadList()
+        { //var serviceResponse = await Client.GetFromJsonAsync<ServiceResponse<List<UserDTO>>>("api/User/Users");
+            try
+            {
+                UserList = await Client.GetServiceResponseAsync<List<UserDTO>>("api/User/Users", true);
+            }
+            catch (ApiException ex)
+            {
+                await ModalManager.ShowMessageAsync("Api Exception", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                await ModalManager.ShowMessageAsync("Exception", ex.Message);
+            }
+            //if (serviceResponse.Success) userList = serviceResponse.Value;
+        }
+
+    }
+}
